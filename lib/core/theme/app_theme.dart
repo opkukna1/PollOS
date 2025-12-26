854c67c53f3e11c24b1fc323fcbc05f2efee908e
opkukna1 <Opsiddh41@gmail.com>
2025-12-26 17:44:09 +0530

    Create app_theme.dart

---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: Colors.deepOrange,
+      brightness: Brightness.light,
+      surface: Colors.grey.shade50,
+    ),
+    textTheme: GoogleFonts.poppinsTextTheme(), // Modern Font
+    cardTheme: CardTheme(
+      elevation: 2,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+    ),
+    appBarTheme: const AppBarTheme(
+      centerTitle: true,
+      elevation: 0,
+      backgroundColor: Colors.white,
+      foregroundColor: Colors.black87,
+    ),
+  );
+}
